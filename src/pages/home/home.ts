10ace4df92a147abf558c0585c1b4b0973a7d2dc
Finn <fnn.flts@yahoo.de>
2018-09-29 13:39:04 +0200

    added sendMessage and authors

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,12 +8,39 @@ import { AngularFireDatabase } from 'angularfire2/database';
   templateUrl: 'home.html'
 })
 export class HomePage {
+
   public obsable;
+  private author1;
+  
   constructor(
     public navCtrl: NavController,
     public db: AngularFireDatabase
     ) {
-      this.obsable = db.list("/items").valueChanges();
-      this.obsable.subscribe(console.log);
+      this.downloadFirebase();
+  }
+
+  //download data from Google Firebase cloud
+  downloadFirebase(){
+    this.obsable = this.db.list("/items").valueChanges();
+    this.obsable.subscribe(console.log);
+  }
+
+  //upload data to Google Firebase cloud
+  uploadFirebase(){
+    const items = this.db.list("/items");
+
+    if(this.author == "a1")
+      this.author1 = "author1";
+    else if(this.author == "a2")
+      this.author1 = "author2";
+
+    if(this.author1 != null)
+      items.push({author: this.author1, content: this.msgContent})
+  }
+
+  //upload data to Google Firebase on click (btn) via "uploadFirebase()"
+  send(){
+    if(this.author != null && this.msgContent != null)
+      this.uploadFirebase();
   }
 }
