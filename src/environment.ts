aaf263e038a85d8e64349e9aff692d80f88b391a
SkinxH <hugo.doering@bluewin.ch>
2018-09-30 11:09:39 +0200

    nironment

--- src/environment.ts
+++ src/environment.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyB6jSMXr9YIu5CtajerFLEi5WZhRiWvFEc",
-    authDomain: "ou-est-la-fete.firebaseapp.com",
-    databaseURL: "https://ou-est-la-fete.firebaseio.com",
-    projectId: "ou-est-la-fete",
-    storageBucket: "ou-est-la-fete.appspot.com",
-    messagingSenderId: "194701757787"
+  apiKey: "AIzaSyC7oDuAGn7DssPsYmvIm0-U8XPDww66iX0",
+  authDomain: "ou-est-la-fete-4de43.firebaseapp.com",
+  databaseURL: "https://ou-est-la-fete-4de43.firebaseio.com",
+  projectId: "ou-est-la-fete-4de43",
+  storageBucket: "ou-est-la-fete-4de43.appspot.com",
+  messagingSenderId: "143931009310"
   };
