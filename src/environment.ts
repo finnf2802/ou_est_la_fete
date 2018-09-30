3424fb645a9920ecaf5233f7b705bf177da08b69
finnf2802 <32400059+finnf2802@users.noreply.github.com>
2018-09-30 16:32:08 +0200

    removed database api relevant informations

--- src/environment.ts
+++ src/environment.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyC7oDuAGn7DssPsYmvIm0-U8XPDww66iX0",
-  authDomain: "ou-est-la-fete-4de43.firebaseapp.com",
-  databaseURL: "https://ou-est-la-fete-4de43.firebaseio.com",
-  projectId: "ou-est-la-fete-4de43",
-  storageBucket: "ou-est-la-fete-4de43.appspot.com",
-  messagingSenderId: "143931009310"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
   };
